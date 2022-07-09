e23c5fd2d85dcc0c9b66a9f847647fe28917b6c8
Deepak <kolaparthis.ug20.cse@nitp.ac.in>
2022-07-09 12:50:44 +0530

    Assignment 3(User Authentication)

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -34,4 +34,15 @@ exports.jwtPassport = passport.use(new JwtStatergy(opts, (jwt_payload, done) => 
     });
 }));
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+exports.verifyAdmin = (req, res, next) => {
+    if(req.user.admin){
+        next();
+    } 
+    else{
+        var err = new Error("You are not authorized to perform this operation!");
+        err.status = 403;
+        return next(err);
+    }
+};
