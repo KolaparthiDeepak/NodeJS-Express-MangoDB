1f2820db4da8db2d1d7b5b123f23e97c9f6cc83c
Deepak <kolaparthis.ug20.cse@nitp.ac.in>
2022-07-11 17:11:40 +0530

    Assignment-4(Backend as a Server)

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,6 +15,7 @@ var dishRouter = require('./routes/dishRouter');
 var promotionRouter = require('./routes/promotionRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var uploadRouter = require('./routes/uploadRouter');
+var favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
@@ -67,6 +68,7 @@ app.use('/dishes', dishRouter);
 app.use('/promotions', promotionRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
