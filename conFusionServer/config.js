87773c682877ffe68fc51c8a913f1269a42bca8a
Deepak <kolaparthis.ug20.cse@nitp.ac.in>
2022-07-11 12:32:42 +0530

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://127.0.0.1:27017/conFusion'
+    'mongoUrl' : 'mongodb://127.0.0.1:27017/conFusion',
+    'facebook' : {
+        clientId: '1720251508308424',
+        clientSecret: '4767faf5edb6900b9a14c48daf87a948'
+    }
 }
